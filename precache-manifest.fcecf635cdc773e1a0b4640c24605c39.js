1da80fbaf7b2105325d685d3ba409901f763b93f
deep0122 <deeppatel0122@gmail.com>
2020-02-26 20:17:17 -0500

    Updates

---
+++ precache-manifest.fcecf635cdc773e1a0b4640c24605c39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02403d79d466bd169e9d0d1d5e316424",
+    "url": "/venuelocator/index.html"
+  },
+  {
+    "revision": "3c6ac2a4ce0f0cb5521b",
+    "url": "/venuelocator/static/css/main.13ab466a.chunk.css"
+  },
+  {
+    "revision": "4780c2fb299b0095427d",
+    "url": "/venuelocator/static/js/2.b265b276.chunk.js"
+  },
+  {
+    "revision": "44ceb747848182a141b24054c3e40f68",
+    "url": "/venuelocator/static/js/2.b265b276.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3c6ac2a4ce0f0cb5521b",
+    "url": "/venuelocator/static/js/main.6621b4c6.chunk.js"
+  },
+  {
+    "revision": "d8ac6a474b153e855ecf",
+    "url": "/venuelocator/static/js/runtime-main.6955872c.js"
+  }
+]);
